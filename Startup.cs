b00e2fa9e3f849c6662131a0eaf5dc756450f830
yfkheada <yfkheada@dimensions-healthcare.com>
2016-03-23 20:41:54 +0200

    test

--- Startup.cs
+++ Startup.cs
@@ -1,16 +1,40 @@
 using Microsoft.AspNet.Builder;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace HelloWeb
+// Note that this sample will not run. It is only here to illustrate usage patterns.
+
+namespace SampleStartups
 {
-    public class Startup
+    public class StartupHelloWorld
     {
-        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
         {
-            loggerFactory.AddConsole();
-            app.UseIISPlatformHandler();
-            app.UseStaticFiles();
-            app.UseWelcomePage();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+
+        // Entry point for the application.
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseDefaultConfiguration(args)
+                .UseStartup<StartupHelloWorld>()
+                .Build();
+
+            host.Run();
         }
     }
 }
